80c91fee4464bb15e7cc59f46fbd1c96057e59a3
Anton Krylov <krylov@cg.ru>
2019-02-27 14:05:59 +0300

    quick fix

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ cp /apps/* standalone/deployments/ 2>/dev/null || :
 cp /standalone.xml standalone/configuration/ 2>/dev/null || :
 
 # TODO workaround for debug disabled on Linux
-echo '#JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=n"' >> bin/standalone.conf
+echo 'JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=n"' >> bin/standalone.conf
 
 # Start the standalone server
 bin/standalone.sh --read-only-server-config=standalone.xml
